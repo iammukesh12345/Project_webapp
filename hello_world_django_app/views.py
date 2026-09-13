7479925524017b9856936dd9e12911efdac01877
Mukesh Thakre <mukeshthakre015@gmail.com>
2026-09-13 20:30:33 +0000

    Add Django application and Docker configuration

--- hello_world_django_app/views.py
+++ hello_world_django_app/views.py
@@ -1,5 +1,13 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+
+def home(request):
+    return HttpResponse("<h1>Welcome to My Django Application</h1>")
 
 
 def hello_world(request):
     return HttpResponse("<html><body>Hello World</body></html>")
+
+
+def health_check(request):
+    return JsonResponse({"status": "healthy"})
